de6969c0874adcccfaaf3a5fc10c8ebb64a10a67
Guido <molido@hotmail.it>
2020-06-24 16:24:07 +0200

    Secondo Commit

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
+
+// assegnazione delle variabili
 var invia = document.getElementById("invia");
 var lista = ["Fermi", "Riccardi", "Creanza", "Fardi", "Sacca"]
 
+// trascrizione della lista
+document.getElementById("elenco").innerHTML = lista;
+
+
+// pulsante invia
 invia.addEventListener("click", function () {
 
+  // inserimento input
   var cognome = document.getElementById('cognome').value;
   lista.push(cognome);
+
   console.log(lista);
 
-  for (var i = 0; i < lista.length; i++) {
-    while (true) {
+  // assortimento in ordine alfabetico
+  lista.sort();
 
-    }
-  }
+  console.log(lista);
+
+  // sovrascrittura nuova lista
+  document.getElementById("elenco").innerHTML = lista;
+
+
+  // for (var i = 0; i < lista.length; i++) {
+  //   while (true) {
+  //
+  //   }
+  // }
 });
